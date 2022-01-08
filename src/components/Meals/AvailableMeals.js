f8860b8d66d20eb8cbdc871899c89fd8757682bb
JanSzalanski <jan@szalanski.eu>
2022-01-08 23:05:47 +0100

    added feching data from backend database via http (useEffect & useState)

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import classes from "./AvailableMeals.module.css";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
+
+//https://react-sample3-default-rtdb.europe-west1.firebasedatabase.app/
 
 const DUMMY_MEALS = [
   {
@@ -31,7 +33,30 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch("https://react-sample3-default-rtdb.europe-west1.firebasedatabase.app/meals.json");
+      const responseData = await response.json();
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+
+      setMeals(loadedMeals);
+    };
+
+    fetchMeals();
+  }, []);
+
+  const mealsList = meals.map((meal) => (
     <MealItem key={meal.id} id={meal.id} name={meal.name} description={meal.description} price={meal.price} />
   ));
   return (
